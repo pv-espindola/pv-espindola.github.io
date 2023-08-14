a327a85cfd62191e620ce86b1ef0b4ef47265edf
Paulo Victor Espíndola <pv.owns@gmail.com>
2023-08-14 14:26:38 -0300

    version fixed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,11 +39,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "7559673711617a8ab33f39f7cfcbc8f0
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "100127aea1e49d57fbe093eae41d7d4e",
-"/": "100127aea1e49d57fbe093eae41d7d4e",
+"index.html": "6eddeb81bf54de0d7852ce2232f4e349",
+"/": "6eddeb81bf54de0d7852ce2232f4e349",
 "main.dart.js": "f088408df088abe76dac4e0e22b1fe23",
 "manifest.json": "28f383a2d95b0e3658be58990600bd89",
-"version.json": "a030c0263617955bb1ab86ba72169763"};
+"version.json": "d35212705485fcc47b15670fef6f5f7b"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
